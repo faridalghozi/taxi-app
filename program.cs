8c4eb7eeef353b86ebfa97904474981cb156da1c
faridalghozi <faridalghozi@gmail.com>
2020-03-21 14:47:23 +0700

    Membuat Class Taxi

---
+++ program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TugasTaxi
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			// membuat objek dari class taxi
+
+			Taxi taxi = new Taxi();
+
+
+			// mengeset nilai properties
+			taxi.DriverName = "Untung Dwi Handoko";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			// memanggil method 
+			taxi.TaxiInfo();
+			taxi.PickupPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+
+	}
+}
